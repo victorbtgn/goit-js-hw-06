75f5fb52d669b6b5e58d5619444d9773597d4930
Victor Batigin <bixs85@gmail.com>
2020-02-17 22:28:49 +0200

    fix 4 task

--- js/task-04.js
+++ js/task-04.js
@@ -3,7 +3,7 @@ console.log(`Task 04`);
 import users from './users.js';
 
 const getInactiveUsers = users => {
-  return users.filter(user => user.isActive === false);
+  return users.filter(user => !user.isActive);
 };
 
 console.log(getInactiveUsers(users));
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
 console.log(`Task 08`);
+
+import users from './users.js';
+
+const getUsersWithFriend = (users, friendName) => {
+  return users
+    .filter(user => {
+      user.friends.forEach(friend => friend === friendName);
+    })
+    .map(user => user.name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker'));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+// [ 'Elma Head', 'Sheree Anthony' ]
